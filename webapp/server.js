fb8e01d564e4768207063c4fce88d561407349a3
chibuzor christian <gigzblog@gmail.com>
2017-11-04 21:38:00 +0100

    Update server.js

--- webapp/server.js
+++ webapp/server.js
@@ -90,5 +90,5 @@ app.post('/studentdetails', urlencodedParser,function(req, res){
 
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Node webapp is running on port', app.get('port'));
 });
